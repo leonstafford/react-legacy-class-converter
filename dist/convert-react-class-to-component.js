9f30656d8b46342a51ca0ceafc5a6a7d766ef37b
Leon Stafford <me@ljs.dev>
2023-04-06 20:49:07 +1000

    fix dist;update vsix

---
+++ dist/convert-react-class-to-component.js
@@ -0,0 +1,44 @@
+export const convertReactClassToComponent = sourceCode => {
+  // Replace createReactClass with React.Component
+  let convertedText = sourceCode.replace(/var\s+(\w+)\s+=\s+createReactClass\({/g, 'class $1 extends React.Component {');
+
+  // Replace the propTypes section with static propTypes
+  convertedText = convertedText.replace(/propTypes:\s*{/, 'static propTypes = {');
+
+  // Replace function declarations with arrow functions
+  convertedText = convertedText.replace(/(\w+)\s*:\s*function\s*\(/g, '$1 = (');
+
+  // Replace this keyword with this.state
+  convertedText = convertedText.replace(/this\.(props|state)\./g, 'this.');
+
+  // Replace createReactClass with React.Component
+  convertedText = convertedText.replace(/createReactClass\({/g, 'Component {');
+
+  // Add the render method if it's missing
+  if (!convertedText.includes('render()')) {
+    convertedText += '\n  render() {\n    return null;\n  }\n';
+  }
+
+  // Replace this.state with this
+  convertedText = convertedText.replace(/this\.state\./g, 'this.');
+
+  // Replace refs with React.createRef()
+  convertedText = convertedText.replace(/React\.createRef\(\)/g, 'createRef()');
+
+  // Replace mixins with regular class methods
+  convertedText = convertedText.replace(/mixins:\s*\[(.*?)\]/gms, (match, mixins) => {
+    const mixinMethods = mixins.split(',').map(mixin => mixin.trim()).map(mixin => `  ${mixin.split('.').pop()}() {}`).join('\n');
+    return `${mixinMethods}\n`;
+  });
+
+  // Replace getDefaultProps with a default value for static defaultProps
+  convertedText = convertedText.replace(/getDefaultProps\(\)\s*{\s*return\s*{([\s\S]*?)\s*};\s*}/gm, 'static defaultProps = {$1};');
+
+  // Replace propTypes with a default value for static propTypes
+  convertedText = convertedText.replace(/propTypes:\s*{([\s\S]*?)}/gm, 'static propTypes = {$1};');
+
+  // Replace getInitialState with a constructor that initializes state
+  convertedText = convertedText.replace(/getInitialState\(\)\s*{\s*return\s*{([\s\S]*?)\s*};\s*}/gm, 'constructor(props) {\n    super(props);\n    this.state = {$1};\n  }');
+
+  return convertedText;
+};
