435708d45c02c899d8cfb519915b97d4c141dc70
Leon Stafford <me@ljs.dev>
2023-04-06 19:51:00 +1000

    cleanup;add tests

---
+++ README.md
@@ -0,0 +1,9 @@
+
+
+
+
+### Dev notes
+
+pakage up the .vsix using the VS Code CLI:
+
+`vsce package` (from `npm install -g vsce`)
---
+++ convert-react-class-to-component.test.js
@@ -0,0 +1,45 @@
+import { convertReactClassToComponent } from './convert-react-class-to-component';
+
+describe('convertReactClassToComponent', () => {
+  it('should convert createReactClass to class extends React.Component', () => {
+    const sourceCode = `
+      var MyComponent = createReactClass({
+        render: function() {
+          return <div>Hello, {this.props.name}!</div>;
+        }
+      });
+    `;
+    const expectedOutput = `
+      class MyComponent extends React.Component {
+        render() {
+          return <div>Hello, {this.props.name}!</div>;
+        }
+      }
+    `;
+    expect(convertReactClassToComponent(sourceCode)).toBe(expectedOutput);
+  });
+
+  it('should add a render method if missing', () => {
+    const sourceCode = `
+      var MyComponent = createReactClass({
+        helloWorld: function() {
+          console.log('Hello, world!');
+        }
+      });
+    `;
+    const expectedOutput = `
+      class MyComponent extends React.Component {
+        render() {
+          return null;
+        }
+
+        helloWorld() {
+          console.log('Hello, world!');
+        }
+      }
+    `;
+    expect(convertReactClassToComponent(sourceCode)).toBe(expectedOutput);
+  });
+
+  // Add more test cases here
+});
